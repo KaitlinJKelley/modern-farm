13458826ea3233d6cbfc6a2841be7559b5c3eb2b
kjk1325e <kaitlinjkelley@gmail.comm>
2021-01-19 15:41:42 -0600

    adjusted plantSeeds function to be a function

--- src/scripts/tractor.js
+++ src/scripts/tractor.js
@@ -1,7 +1,7 @@
 import { addPlant } from "./field"
 import { createAsparagus } from "./seeds/asparagus"
 
-export plantSeeds (plan) => {
+export const plantSeeds = (plan) => {
     for(let i = 0; i < plan.length; i++) {
         for(let j = 0; j < plan[i].length; j++) {
             if (plan[i] === Asparagus) {
